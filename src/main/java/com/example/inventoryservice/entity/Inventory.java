95f3112bad592d3c8d15e83487c7bc376073795c
Oscar Yair Bautista Ortiz <obautist@emeal.nttdata.com>
2026-05-25 12:51:59 -0600

    Se agrega entidad y repositorio

---
+++ src/main/java/com/example/inventoryservice/entity/Inventory.java
@@ -0,0 +1,44 @@
+package com.example.inventoryservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "inventory")
+public class Inventory {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    
+}
